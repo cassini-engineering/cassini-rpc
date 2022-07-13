153df378a7162e801dba069271c62459505f3b5d
Shivam <shivam@sfninety.com>
2022-07-14 00:04:45 +0100

    use correct handler type

--- server.go
+++ server.go
@@ -10,18 +10,17 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/bufbuild/connect-go"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 type Server struct {
 	mux      *http.ServeMux
-	services map[string]*connect.Handler
+	services map[string]http.Handler
 	server   *http.Server
 }
 
-func (s *Server) RegisterHandler(path string, handler *connect.Handler) {
+func (s *Server) RegisterHandler(path string, handler http.Handler) {
 	if handler == nil {
 		return
 	}
@@ -72,6 +71,6 @@ func (s *Server) Serve(port int32) {
 func NewServer() *Server {
 	return &Server{
 		mux:      http.NewServeMux(),
-		services: make(map[string]*connect.Handler),
+		services: make(map[string]http.Handler),
 	}
 }
